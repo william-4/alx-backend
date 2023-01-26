1e7085a9d2514f241460c69ad355fec8f1ed39a3
mwangii_k <93374467+william-4@users.noreply.github.com>
2023-01-26 16:36:39 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,28 +13,28 @@ Methods: put
 """
 
     def __init__(self):
-    """
-    Initialize BasicCache using parent class __init__ method
-    """
+        """
+        Initialize BasicCache using parent class __init__ method
+        """
         super().__init__()
 
     def put(self, key, item):
-    """
-    Adds an item to our dict from cache_data
-    """
+        """
+        Adds an item to our dict from cache_data
+        """
         if key is None or item is None:
-            pass
+           pass
         else:
-            self.cache_data[key] = item
-            if (len(self.cache_data) > super().MAX_ITEMS):
-                popped = list(self.cache_data.keys())[0]
-                print("DISCARD: {}".format(popped))
-                del self.cache_data[popped]
+           self.cache_data[key] = item
+           if (len(self.cache_data) > super().MAX_ITEMS):
+               popped = list(self.cache_data.keys())[0]
+               print("DISCARD: {}".format(popped))
+               del self.cache_data[popped]
 
     def get(self, key):
-    """
-    Retrieves an item from our dict cache_data
-    """
+        """
+        Retrieves an item from our dict cache_data
+        """
         if key is None or key not in self.cache_data.keys():
             pass
         else:
