67ee8a3350500dc66245b70ff0e8119cc67209f0
mwangii_k <93374467+william-4@users.noreply.github.com>
2023-01-26 16:29:23 +0300

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -13,29 +13,29 @@ class LIFOCache(BaseCaching):
     """
 
     def __init__(self):
-      """
-          Initialize BasicCache using parent class __init__ method
-          """
-      super().__init__()
+        """
+        Initialize BasicCache using parent class __init__ method
+        """
+        super().__init__()
 
     def put(self, key, item):
-      """
-          Adds an item to our dict from cache_data
-          """
-      if key is None or item is None:
-        pass
-      else:
-        self.cache_data[key] = item
-        if (len(self.cache_data) > super().MAX_ITEMS):
-          popped = list(self.cache_data.keys())[super().MAX_ITEMS - 1]
-          print("DISCARD: {}".format(popped))
-          del self.cache_data[popped]
+        """
+        Adds an item to our dict from cache_data
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+            if (len(self.cache_data) > super().MAX_ITEMS):
+                popped = list(self.cache_data.keys())[super().MAX_ITEMS - 1]
+                print("DISCARD: {}".format(popped))
+                del self.cache_data[popped]
 
     def get(self, key):
-      """
-          Retrieves an item from our dict cache_data
-          """
-      if key is None or key not in self.cache_data.keys():
+    """
+    Retrieves an item from our dict cache_data
+    """
+    if key is None or key not in self.cache_data.keys():
         pass
       else:
         return (self.cache_data[key])
