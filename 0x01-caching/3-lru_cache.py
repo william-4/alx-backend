1df0b8c39eee7d52d597b4f15a1cbd1660443483
mwangii_k <93374467+william-4@users.noreply.github.com>
2023-01-26 17:00:53 +0300

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" LRUCache module that inherits from BaseCaching
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """A caching system using LRU - Least Recently Used.
+    """
+    def __init__(self):
+        """Initializes cache data.
+        """
+        super().__init__()
+        self.age = list(self.cache_data.keys())
+
+    def put(self, key, item):
+        """Add key value data to the cache.
+        """
+        if key is None or item is None:
+            pass
+
+        self.cache_data[key] = item
+        if key in self.age:
+            self.age.remove(key)
+            self.age.append(key)
+        else:
+            self.age.append(key)
+
+        if len(self.cache_data) > super().MAX_ITEMS:
+            popped = self.age[0]
+            print("Discard: {}".format(popped))
+            del self.cache_data[popped]
+            self.age.remove(popped)
+
+    def get(self, key):
+        """ Retrieves key's value.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        self.age.remove(key)
+        self.age.append(key)
+        return self.cache_data[key]
