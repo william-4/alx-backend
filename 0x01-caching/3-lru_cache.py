02cd2284471b3c61d982dfdb504ae01beea04957
william-4 <mwangiiwilliam@gmail.com>
2023-01-28 07:07:38 +0300

    lru algorithm implementation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -27,7 +27,7 @@ class LRUCache(BaseCaching):
         else:
             self.age.append(key)
 
-        if len(self.cache_data) > super().MAX_ITEMS:
+        if len(self.age) > super().MAX_ITEMS:
             popped = self.age[0]
             print("Discard: {}".format(popped))
             del self.age[0]
@@ -42,7 +42,9 @@ class LRUCache(BaseCaching):
             self.age.remove(key)
             self.age.append(key)
         else:
-            if len(self.age) > super().MAX_ITEMS:
-                del self.age[0]
             self.age.append(key)
+
+        if len(self.age) > super().MAX_ITEMS:
+            del self.age[0]
+
         return self.cache_data[key]
